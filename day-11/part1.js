f0847b2b6387878f40f7dbc4c52eeb3ab361749d
David Wögerbauer <56993269+d-woegerbauer@users.noreply.github.com>
2023-12-11 07:58:26 +0000

    Add day 11 of aoc 2023

---
+++ day-11/part1.js
@@ -0,0 +1,86 @@
+const fs = require("fs");
+
+let input = fs
+  .readFileSync("./input.txt", "utf8")
+  .replaceAll("\r", "")
+  .split("\n")
+  .map((x) => x.split(""));
+
+console.log(input);
+
+let columnIndexes = [];
+let rowIndexes = [];
+
+input.forEach((line, index) => {
+  let isOnlyDot = true;
+  line.forEach(element => {
+    if(element != "."){
+      isOnlyDot = false;
+    }
+  });
+  if(isOnlyDot){
+    rowIndexes.push(index);
+  }
+});
+
+for(let i = 0; i < input[0].length; i++){
+  let isOnlyDot = true;
+  for(let j = 0; j < input.length; j++){
+    if(input[j][i] != "."){
+      isOnlyDot = false;
+    }
+  }
+  if(isOnlyDot){
+    columnIndexes.push(i);
+  }
+}
+
+let newRowIndexes = [];
+let newColumnIndexes = [];
+
+rowIndexes.forEach((row, index) => {
+  newRowIndexes.push(row+index);
+});
+
+columnIndexes.forEach((row, index) => {
+  newColumnIndexes.push(row+index);
+});
+
+newRowIndexes.forEach(index => {
+  input.splice(index, 0, new Array(input[0].length).fill("."));
+})
+
+for(let i = 0; i < input.length; i++){
+  for(let j = 0; j < input[i].length; j++){
+    if(newColumnIndexes.includes(j)){
+      input[i].splice(j, 0, ".");
+    }
+  }
+}
+
+let coordinates = [];
+
+input.forEach((line, i) => {
+  line.forEach((element, j) => {
+    if(element == "#"){
+      coordinates.push([i, j]);
+    }
+  })
+})
+
+let permutation = coordinates.flatMap(
+  (v, i) => coordinates.slice(i+1).map( w => [v, w] )
+);
+
+let sum = 0;
+
+permutation.forEach((pair) => {
+  let [x1, y1] = pair[0];
+  let [x2, y2] = pair[1];
+  let xDiff = Math.abs(x1 - x2);
+  let yDiff = Math.abs(y1 - y2);
+  sum += xDiff + yDiff;
+}
+);
+
+console.log(sum);
