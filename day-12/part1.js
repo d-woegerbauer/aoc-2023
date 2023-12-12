47a638d1e6329c4d8327832c31ce2f162af193bb
David Wögerbauer <56993269+d-woegerbauer@users.noreply.github.com>
2023-12-12 21:15:05 +0100

    Work at day 12 of aoc 2023

--- day-12/part1.js
+++ day-12/part1.js
@@ -4,47 +4,61 @@ let input = fs
   .readFileSync("./input.txt", "utf8")
   .replaceAll("\r", "")
   .split("\n")
-  .map((x) => x.split(" ").map((y) => y.replaceAll(",", "").split("")));
+  .map((x) => x.split(" ").map((y) => {
+    if(y.includes(",")){
+      return y.split(",");
+    } else {
+      return y.split("");
+    }
+  }));
 
 
-  console.log(input);
 
   let permutations = [];
 
-  // make a permutation of the input which replaces each "?" with a "." or "#", there are multiple ? in each line of the input
   input.forEach(line => {
-    // the permutation should replace every "?" with a "." or "#"
     let permutation = [];
-    function replaceQuestionMarks(line) {
-      let questionMarkIndex = line.indexOf("?");
-      if (questionMarkIndex > -1) {
-        let lineCopy = [...line];
-        lineCopy[questionMarkIndex] = ".";
-        replaceQuestionMarks(lineCopy);
-        lineCopy[questionMarkIndex] = "#";
-        replaceQuestionMarks(lineCopy);
-      } else {
-        permutation.push(line);
+    let questionMarks = line[0].filter((x) => x == "?").length;
+    let possiblePermutations = Math.pow(2, questionMarks);
+
+    for(let i = 0; i < possiblePermutations; i++) {
+      let binary = i.toString(2).padStart(questionMarks, "0");
+      let permutationLine = [...line[0]];
+
+      for(let j = 0; j < binary.length; j++) {
+        permutationLine[permutationLine.indexOf("?")] = binary[j] == "0" ? "." : "#";
       }
+
+      permutation.push(permutationLine);
     }
-    replaceQuestionMarks(line[0]);
+
     permutations.push(permutation);
   });
-
-  console.log(permutations[5]);
 
   let sum = 0;
 
   permutations.forEach((permutation, index) => {
-    let possibleDamaged = input[index][1];
 
     permutation.forEach((line) => {
+      let possibleDamaged = [...input[index][1]];
+      let notMatched = false;
+
+
       line.join("").split(".").forEach((subsequence) => {
-        if(subsequence.length == possibleDamaged.shift() && index == 5) {
-          sum++;
+        if(subsequence.length == possibleDamaged[0]) {
+          possibleDamaged.shift();
+        } else if(subsequence != ""){
+          notMatched = true;
         }
       });
+
+      if(possibleDamaged.length == 0 && !notMatched) {
+          // console.log(line.join(""));
+        sum++;
+      }
     });
+
+    // console.log("sum", sum)
   }
   );
 
--- day-12/part2.js
+++ day-12/part2.js
@@ -4,95 +4,64 @@ let input = fs
   .readFileSync("./input.txt", "utf8")
   .replaceAll("\r", "")
   .split("\n")
-  .map((x) => x.split(""));
-
-console.log(input);
-
-let columnIndexes = [];
-let rowIndexes = [];
-
-input.forEach((line, index) => {
-  let isOnlyDot = true;
-  line.forEach(element => {
-    if(element != "."){
-      isOnlyDot = false;
+  .map((x) => x.split(" ").map((y) => {
+    if(y.includes(",")){
+      let subArray = new Array(5).fill(y);
+      return subArray.join(",").split(",");
+    } else {
+      let subArray = new Array(5).fill(y);
+      return subArray.join("?").split("");
     }
+  }));
+
+  console.log(input);
+
+  let permutations = [];
+
+  input.forEach(line => {
+    let permutation = [];
+    let questionMarks = line[0].filter((x) => x == "?").length;
+    let possiblePermutations = Math.pow(2, questionMarks);
+
+    for(let i = 0; i < possiblePermutations; i++) {
+      let binary = i.toString(2).padStart(questionMarks, "0");
+      let permutationLine = [...line[0]];
+
+      for(let j = 0; j < binary.length; j++) {
+        permutationLine[permutationLine.indexOf("?")] = binary[j] == "0" ? "." : "#";
+      }
+
+      permutation.push(permutationLine);
+    }
+
+    permutations.push(permutation);
   });
-  if(isOnlyDot){
-    rowIndexes.push(index);
-  }
-});
 
-for(let i = 0; i < input[0].length; i++){
-  let isOnlyDot = true;
-  for(let j = 0; j < input.length; j++){
-    if(input[j][i] != "."){
-      isOnlyDot = false;
-    }
-  }
-  if(isOnlyDot){
-    columnIndexes.push(i);
-  }
-}
+  let sum = 0;
 
-// let newRowIndexes = [];
-// let newColumnIndexes = [];
+  permutations.forEach((permutation, index) => {
 
-// rowIndexes.forEach((row, index) => {
-//   newRowIndexes.push(row+index);
-// });
+    permutation.forEach((line) => {
+      let possibleDamaged = [...input[index][1]];
+      let notMatched = false;
 
-// columnIndexes.forEach((row, index) => {
-//   newColumnIndexes.push(row+index);
-// });
 
-// newRowIndexes.forEach(index => {
-//   input.splice(index, 0, new Array(input[0].length).fill("."));
-// })
+      line.join("").split(".").forEach((subsequence) => {
+        if(subsequence.length == possibleDamaged[0]) {
+          possibleDamaged.shift();
+        } else if(subsequence != ""){
+          notMatched = true;
+        }
+      });
 
-// for(let i = 0; i < input.length; i++){
-//   for(let j = 0; j < input[i].length; j++){
-//     if(newColumnIndexes.includes(j)){
-//       input[i].splice(j, 0, ".");
-//     }
-//   }
-// }
-
-let coordinates = [];
-
-input.forEach((line, i) => {
-  line.forEach((element, j) => {
-    if(element == "#"){
-      coordinates.push([i, j]);
-    }
-  })
-})
-
-let permutation = coordinates.flatMap(
-  (v, i) => coordinates.slice(i+1).map( w => [v, w] )
-);
-
-let sum = 0;
-
-permutation.forEach((pair) => {
-  let [x1, y1] = pair[0];
-  let [x2, y2] = pair[1];
-  let xDiff = Math.abs(x1 - x2);
-  let yDiff = Math.abs(y1 - y2);
-  rowIndexes.forEach((row, index) => {
-      if(x1 < row && row < x2 || x2 < row && row < x1){
-        xDiff += 999999;
+      if(possibleDamaged.length == 0 && !notMatched) {
+          // console.log(line.join(""));
+        sum++;
       }
-    }
+    });
+
+    // console.log("sum", sum)
+  }
   );
-  columnIndexes.forEach((column, index) => {
-      if(y1 < column && column < y2 || y2 < column && column < y1){
-        yDiff += 999999;
-      }
-    }
-  );
-  sum += xDiff + yDiff;
-}
-);
 
 console.log(sum);
